39ce1482c2ec0c20e0544bba8389fcdfc4b57f2a
Drashtipatel07 <pateldrashti748@gmail.com>
2026-05-23 19:02:06 +0530

    updated website

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,8 +13,10 @@ function Navbar() {
                 <ul className={isOpen ? "nav-links active" : "nav-links"}>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Skills</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
+                    
                 </ul>
             </div>
         </nav>
